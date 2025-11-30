2782031391ced294b738c0941a7ce324765aab87
Zhenyi Qi <zyqi2011@gmail.com>
2025-11-30 10:35:04 -0800

    chore: add cluster setup file

---
+++ scripts_zhenyi/set_up.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+git clone https://github.com/zhenyiqi/LongLive.git
+cd LongLive
+# install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash Miniconda3-latest-Linux-x86_64.sh
+
+source ~/.bashrc
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+# create env
+conda create -n longlive python=3.10 -y
+conda activate longlive
+
+# install dependencies
+#
+conda install nvidia/label/cuda-12.4.1::cuda
+conda install -c nvidia/label/cuda-12.4.1 cudatoolkit
+pip install --upgrade
+pip install torch==2.8.0 torchvision==0.23.0 --index-url https://download.pytorch.org/whl/cu128
+pip install -r requirements.txt
+pip install nvidia-pyindex --no-build-isolation
+pip install flash-attn --no-build-isolation
+
+# download huggingface-cli
+pip install huggingface_hub
+hf download Wan-AI/Wan2.1-T2V-1.3B --local-dir wan_models/Wan2.1-T2V-1.3B
+hf download Efficient-Large-Model/LongLive --local-dir longlive_models
